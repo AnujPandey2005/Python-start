791756818c244b6998cd1f642954035f5a82dae2
anujpandey <ap9089511@gmail.com>
2025-10-09 21:18:32 +0530

    assignment operators

---
+++ 9 assignment operators.py
@@ -0,0 +1,22 @@
+# assignment operators
+
+# it helps to assign the value to a variable
+# its add operator type of addition in value of previous score
+a = 10
+a+=2
+print(a)
+
+# its subtract operator type of subtraction in value of previous score
+b = 20
+b-= 5
+print(b)
+
+# its multiply operator type of multiplication in value of previous score
+c = 5
+c*=2
+print(c)
+
+# its divide operator type of division in value of previous score
+d = 20
+d/= 2
+print(d)
